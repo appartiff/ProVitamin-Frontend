c7c48c51a078b783a48f0fbe605b9c38b4404587
Marius Årnes <marius.appa@gmail.com>
2020-02-26 20:51:17 +0100

    Updated fontawesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,91 +1,80 @@
-
 export default {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'Tita' || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-    '~/assets/styles/main.css'
-  ],
-  
-  
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '~/plugins/directives.js'
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/style-resources',
-    [
-      'nuxt-fontawesome', {
-      imports: [
-        {
-          set: '@fortawesome/free-solid-svg-icons',
-          icons: ['faSearch','faUser','faShoppingBasket','faChevronDown','faChevronUp']
-        },
-        // {
-        //   set:'@fortawesome/free-brands-svg-icons',
-        //   icons: ['fab']
-        // }
-      ]
-    }],
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
-  ],
-  styleResources: {
-    scss: [
-    ]
-  },
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-  },
-  /*
-  ** Build configuration
-  */
-  build: {
-    postcss: {
-      preset: {
-        features: {
-          customProperties: false
-        }
-      }
+    mode: 'universal',
+    /*
+    ** Headers of the page
+    */
+    head: {
+        title: 'Tita' || '',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+        ]
     },
     /*
-    ** You can extend webpack config here
+    ** Customize the progress-bar color
     */
-    extend (config, ctx) {
+    loading: {color: '#fff'},
+    /*
+    ** Global CSS
+    */
+    css: [
+        '~/assets/styles/main.css'
+    ],
+
+
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [
+        '~/plugins/directives.js'
+    ],
+    /*
+    ** Nuxt.js dev-modules
+    */
+    buildModules: [
+        ['@nuxtjs/fontawesome',
+            {
+                suffix: true,
+                icons: {
+                    // list the icons you want to add, not listed icons will be tree-shaked
+                    solid: ['faSearch', 'faUser', 'faShoppingBasket', 'faChevronDown', 'faChevronUp']
+                }
+            }],
+        // Doc: https://github.com/nuxt-community/eslint-module
+        '@nuxtjs/eslint-module'
+    ],
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios'
+    ],
+    /*
+    ** Axios module configuration
+    ** See https://axios.nuxtjs.org/options
+    */
+    axios: {},
+    /*
+    ** Build configuration
+    */
+    build: {
+        analyze: true,
+        postcss: {
+            preset: {
+                features: {
+                    customProperties: false
+                }
+            }
+        },
+        /*
+        ** You can extend webpack config here
+        */
+        extend(config, ctx) {
+        }
     }
-  }
 }
